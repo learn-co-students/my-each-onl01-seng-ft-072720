a3e57b4102b3332135ee1d16b75457c1e2f5504b
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 19:03:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
+#require 'pry'
+
 def my_each(collection)
   if collection.length != 0
   i = 0
   while i < collection.length
     yield(collection[i])
     i += 1
+    #binding.pry
     end
   end
   collection
